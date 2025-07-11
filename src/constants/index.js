31e9fed2be156b95a189afa283d073b41cb7428f
Timur Jalilov <timurjalilov1@gmail.com>
2025-07-11 11:20:46 +0100

    fix: update experience section with relevant info

--- src/constants/index.js
+++ src/constants/index.js
@@ -170,31 +170,50 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review:
-      'Joining Adaptive through the graduate program and continuing to become a seasoned developer working on delivering cutting edge frontends for institutional trading applications for over 3 years has given me a excellent technical foundation and expert level skills.',
+    review: `Taking the time to level up my technical and soft skills on the way to becoming a Senior/Tech Lead, I have been diving deep into the Professional
+       Frontend Developer teaching path on Frontend Masters. Covering a wide range advanced topics like JavaScript closures, asynchronicity, React portals, custom
+        hooks, TypeScript advanced types, generics and much more.`,
+    imgPath: '/images/logos/frontend-masters.webp',
+    logoPath: '/images/logos/frontend-masters-small.webp',
+    title: 'Sabbatical/Upskilling',
+    date: 'October 2024 - Present',
+    responsibilities: [
+      `Completing the Beginner Frontend Developer path, taking the initiative to build upon and solidify my foundational knowledge. Built multiple standalone
+       HTML and CSS centric projects using the latest industry standards and methodologies, which culminated in a final Wordle clone project using HTML,
+       CSS and Javascript.`,
+      `Diving deep into advanced JavaScript, understanding how everything works under the hood, starting from how JS code is executed using the Thread of
+       Execution, the Execution Context, Memory and the Call Stack. Covering Higher Order Functions, Callbacks, Closure, Single Threaded vs Asynchronous and how
+       the browser handles the Asynchronicity. And finally finishing off with Promises and Class and Object Oriented JS.`,
+      `Covering advanced React concepts, starting from builtin Hooks, utilising Custom Hooks for reusability, React Portals for rendering outside of the application root
+       to help implement Modals, Error Boundaries for elegant error handling and logging. Implementing Unit Tests with Vitest and using Test Driven Development to write
+       tests ahead of creating the component and making sure they pass, Mocking the API calls to avoid unnecessary calls and load on the server. Covering the React
+       ecosystem implementing TanStack Router with lazy loading to improve load times, using TanStack Query for asynchronous state management and integration with API
+       calls at set time intervals for dynamic data.`,
+    ],
+  },
+  {
+    review: `Joining Adaptive through the graduate program and going on to become a seasoned developer working on delivering cutting edge frontends for institutional
+       trading applications for over 3 years has given me a excellent technical foundation and expert level skills.`,
     imgPath: '/images/logos/adaptive.svg',
     logoPath: '/images/logos/adaptive-small.svg',
     title: 'Frontend Developer',
     date: 'September 2021 - October 2024',
     responsibilities: [
-      'Worked in a team of a mid and senior frontend developers, building sophisticated, high performance trading UIs using React at the core, React Router for routing, Redux for state management, Websockets to communicate with the backend, AG Grid to display and manage incoming and outgoing trades, ',
-      'Collaborated closely with UI/UX designers to ensure seamless user experiences.',
-      'Optimized web applications for maximum speed and scalability.',
+      `Working in a team of a mid and senior frontend developers, building sophisticated, high performance trading UI's using React and TypeScript at the core,
+       React Router for routing, Redux for state management, Websockets to communicate with the backend, AG Grid to display and manage incoming and outgoing trades,
+       Styled Components for efficient and consistent styling, Jest and React Testing Library for unit tests, Cypress for end to end tests.`,
+      `Working closely with the Backend team to collaborate on the development new api endpoint and the shape of the data. Communicate technical information clearly
+       to explain any Frontend limitations or drawbacks and suggest alternatives. Test and collaborate to ensure speed and reliability with the Backend.`,
+      `Coordinate with the QA team to help improve test automation using Cypress and Playwright, identify potential areas of complexity and failure to prevent
+       future regression`,
+      `Coach and guide Junior developers that join the team, to empower them with the confidence and the knowledge to start contributing to the codebase as soon as possible
+       and start making a impact.`,
+      `Working in a Agile environment with biweekly sprint and monthly productions releases, communicating effectively with the PM and BA to clarify client requirements
+       and identify potential issues and technical and business domain limitations. Collaborating with other Frontend developers to estimate requirements and discuss
+       technical approaches. Perform sprint retrospectives to continuously improve our processes for efficiency and reliability.`,
     ],
   },
-  {
-    review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: '/images/exp2.png',
-    logoPath: '/images/logo2.png',
-    title: 'Full Stack Developer',
-    date: 'June 2020 - December 2023',
-    responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      'Worked with backend engineers to integrate APIs seamlessly with the frontend.',
-      'Contributed to open-source projects that were used with the Docker ecosystem.',
-    ],
-  },
+
   {
     review:
       'Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.',
